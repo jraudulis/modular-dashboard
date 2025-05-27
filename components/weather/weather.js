fdd95e83bf4b39a9efe383c05e04c15dba3f1e28
Juris Raudulis <juris.raudulis@outlook.com>
2025-05-27 13:43:14 +0100

    Uploading project files

---
+++ components/weather/weather.js
@@ -0,0 +1,5 @@
+export function initWeather (container) {
+    container.innerHTML = `
+    <h1> Weather Widget </h1>
+    `;
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+import { initWeather } from "./components/weather/weather";
+
+document.addEventListener('DOMContentLoaded', () =>{
+    initWeather (document.getElementById(weather-container));
+});
