7f9414f2ab122d0cc83b452e2b3655e7bc48d2e2
Juris Raudulis <juris.raudulis@outlook.com>
2025-06-05 15:40:03 +0100

    Adding news functionality

---
+++ components/news/news.js
@@ -0,0 +1,50 @@
+const NEWS_API_KEY = '4ee18cbebdb34b30ab8af59febc9160c';
+const url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${NEWS_API_KEY}`;
+
+async function fetchAndStoreData(container) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const articles = data.articles.slice(0, 5);
+        displayNews(container, articles)
+        console.log(articles);
+    } catch(error) {
+      console.log('Error while fetching', error);
+    }
+}
+
+function displayNews(container, articles) {
+    const list = container.querySelector('.news-list');
+    list.innerHTML = '';
+
+     articles.forEach(({ title, url, urlToImg }) => {
+        const img = document.createElement('img');
+        img.src = urlToImg;
+        img.alt = title;
+        const li = document.createElement('li');
+        li.classList.add('news-item');
+        const a = document.createElement('a');
+        a.classList.add('news-link');
+        a.href = url;
+        a.textContent = title;
+
+        li.appendChild(a);
+        list.appendChild(li);
+        list.appendChild(img);
+     })
+}
+
+export function initNews(container) {
+    container.innerHTML = `
+    <div class="news-widget">
+    <h2>Latest News</h2>
+    <ul class="news-list">
+      <li class="news-item">
+        <a href="#" class="news-link" target="_blank">Loading...</a>
+      </li>
+    </ul>
+  </div>
+ `;
+
+ fetchAndStoreData(container);
+}
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 import { initWeather } from "./components/weather/weather.js";
 import { initTodo } from "./components/todo/todo.js";
 import { initPomodoro } from "./components/pomodoro/pomodoro.js";
+import { initNews } from "./components/news/news.js";
 
 document.addEventListener('DOMContentLoaded', () =>{
     initWeather (document.getElementById('weather-container'));
     initTodo (document.getElementById('todo-container'));
     initPomodoro (document.getElementById('pomodoro-container'));
+    initNews (document.getElementById('news-container'));
 });
